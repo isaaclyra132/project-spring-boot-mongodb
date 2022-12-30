6b7ba56a3d4d791aaa8f4e457f0c31e020fbf5db
isaaclyra <isaac.lyra.junior@gmail.com>
2022-12-30 01:47:52 -0300

    Endpoint para retornar os posts de um usuario

--- workshopmongo/src/main/java/br/isaaclyra/workshopmongo/resources/UserResource.java
+++ workshopmongo/src/main/java/br/isaaclyra/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.isaaclyra.workshopmongo.resources;
 
+import br.isaaclyra.workshopmongo.domain.Post;
 import br.isaaclyra.workshopmongo.domain.User;
 import br.isaaclyra.workshopmongo.dto.UserDTO;
 import br.isaaclyra.workshopmongo.repository.UserRepository;
@@ -59,5 +60,11 @@ public class UserResource {
         obj.setId(id);
         obj = userService.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
